76d7983aac55e63b7b05c2d658572f6dce308ae6
hedeee <hhhttddd@163.com>
2019-01-13 00:47:35 +0800

    前后端分离架构后的联调，mybatisPlus的集成

---
+++ product_parent/product_service_8002/src/main/java/org/hedee/aigou/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package org.hedee.aigou.mapper;
+
+import org.hedee.aigou.domain.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yhptest
+ * @since 2019-01-13
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ product_parent/product_service_8002/src/main/java/org/hedee/aigou/service/IUserService.java
@@ -0,0 +1,16 @@
+package org.hedee.aigou.service;
+
+import org.hedee.aigou.domain.User;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yhptest
+ * @since 2019-01-13
+ */
+public interface IUserService extends IService<User> {
+
+}
